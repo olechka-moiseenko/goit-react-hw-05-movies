90b3d3840a0a0b3b50224a9b32112d4a5256a56e
olechka-moiseenko <73791863+olechka-moiseenko@users.noreply.github.com>
2021-08-11 18:15:25 +0300

    Сделала navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import AppBar from "./components/AppBar/AppBar";
+import Container from "./components/Container/Container";
+import HomeView from "./views/HomeView";
+import AuthorsView from "./views/AuthorsView";
+import BooksView from "./views/BooksView";
+import BookDetailsView from "./views/BookDetailsView";
+import NotFoundView from "./views/NotFoundView";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <AppBar />
+
+      <Switch>
+        <Route path="/" exact>
+          <HomeView />
+        </Route>
+
+        <Route path="/authors">
+          <AuthorsView />
+        </Route>
+
+        <Route path="/books" exact>
+          <BooksView />
+        </Route>
+
+        <Route path="/books/:bookId">
+          <BookDetailsView />
+        </Route>
+
+        <Route>
+          <NotFoundView />
+        </Route>
+      </Switch>
+    </Container>
   );
 }
-
-export default App;
